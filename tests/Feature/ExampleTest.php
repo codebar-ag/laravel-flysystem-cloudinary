bb13f2f7ecb62c45def85c7db2468a0866ca85c4
Ruslan <steiger.ruslan@gmail.com>
2021-05-24 18:21:49 +0200

    Initial release

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CodebarAg\FlysystemCloudinary\Tests\Feature;
+
+use CodebarAg\FlysystemCloudinary\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_does_test()
+    {
+        $this->assertTrue(true);
+    }
+}
