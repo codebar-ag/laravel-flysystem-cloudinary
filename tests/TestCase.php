7e84cb87fcda434b6460232368c499eebc5d7e48
Sebastian Fix <sebastian.fix@codebar.ch>
2023-07-09 12:49:27 +0200

    WIP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,6 @@
 
 namespace CodebarAg\FlysystemCloudinary\Tests;
 
-use Cloudinary\Cloudinary;
 use CodebarAg\FlysystemCloudinary\FlysystemCloudinaryAdapter;
 use CodebarAg\FlysystemCloudinary\FlysystemCloudinaryServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
@@ -21,19 +20,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app): void
     {
         config()->set('database.default', 'testing');
-    }
-
-    public static function setUpBeforeClass(): void
-    {
-        $cloudinary = new Cloudinary([
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'url' => [
-                'secure' => true,
-            ],
-        ]);
-
-        self::$cloudinaryAdapter = new FlysystemCloudinaryAdapter($cloudinary);
     }
 }
